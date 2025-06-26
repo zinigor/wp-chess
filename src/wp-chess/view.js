d4df7aaf215283be0b52b12f7ab5a6c8d5101d74
Igor Zinovyev <zinigor@gmail.com>
2025-06-26 10:23:37 +0300

    Added the chessboard presentation layer.

--- src/wp-chess/view.js
+++ src/wp-chess/view.js
@@ -1,25 +1,74 @@
 /**
- * Use this file for JavaScript code that you want to run in the front-end
- * on posts/pages that contain this block.
- *
- * When this file is defined as the value of the `viewScript` property
- * in `block.json` it will be enqueued on the front end of the site.
- *
- * Example:
- *
- * ```js
- * {
- *   "viewScript": "file:./view.js"
- * }
- * ```
- *
- * If you're not making any changes to this file because your project doesn't need any
- * JavaScript running in the front-end, then you should delete this file and remove
- * the `viewScript` property from `block.json`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#view-script
+ * The chessboard view file.
  */
 
-/* eslint-disable no-console */
-console.log( 'Hello World! (from create-block-wp-chess block)' );
-/* eslint-enable no-console */
+/* global Chessboard */
+
+import '@chrisoakman/chessboardjs/dist/chessboard-1.0.0.js';
+import '@chrisoakman/chessboardjs/dist/chessboard-1.0.0.css';
+
+// Importing images.
+import blackKing from './assets/bK.png';
+import whiteKing from './assets/wK.png';
+import blackQueen from './assets/bQ.png';
+import whiteQueen from './assets/wQ.png';
+import blackRook from './assets/bR.png';
+import whiteRook from './assets/wR.png';
+import blackBishop from './assets/bB.png';
+import whiteBishop from './assets/wB.png';
+import blackKnight from './assets/bN.png';
+import whiteKnight from './assets/wN.png';
+import blackPawn from './assets/bP.png';
+import whitePawn from './assets/wP.png';
+
+( function () {
+	const boards = document.querySelectorAll( '.fen-position-board' ),
+		boardInstances = {};
+	let counter = 1;
+
+	boards.forEach( ( node ) => prepareBoard( node ) );
+
+	function prepareBoard( node ) {
+		const id = 'wp-chess-fen-' + counter;
+		node.setAttribute( 'id', id );
+
+		boardInstances[ counter ] = Chessboard( id, {
+			draggable: false,
+			dropOffBoard: 'trash',
+			sparePieces: false,
+			pieceTheme,
+			position: node.getAttribute( 'data-fen' ),
+		} );
+
+		counter++;
+	}
+
+	function pieceTheme( piece ) {
+		switch ( piece ) {
+			case 'wK':
+				return whiteKing;
+			case 'bK':
+				return blackKing;
+			case 'wQ':
+				return whiteQueen;
+			case 'bQ':
+				return blackQueen;
+			case 'wR':
+				return whiteRook;
+			case 'bR':
+				return blackRook;
+			case 'wB':
+				return whiteBishop;
+			case 'bB':
+				return blackBishop;
+			case 'wN':
+				return whiteKnight;
+			case 'bN':
+				return blackKnight;
+			case 'wP':
+				return whitePawn;
+			case 'bP':
+				return blackPawn;
+		}
+	}
+} )();
